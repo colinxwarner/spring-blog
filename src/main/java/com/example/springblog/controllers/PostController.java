a09a0cd54a8c3bd3618b9aa43f54f899d957a915
Kathleen Weaver <kathleen@kweaver.org>
2021-04-30 14:32:42 -0500

    post views

--- src/main/java/com/example/springblog/controllers/PostController.java
+++ src/main/java/com/example/springblog/controllers/PostController.java
@@ -1,23 +1,39 @@
 package com.example.springblog.controllers;
 
+import com.example.springblog.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
 
+    List<Post> posts = new ArrayList<>();
+
+    public PostController() {
+        posts.add(new Post("Beagles", "my beagles"));
+        posts.add(new Post("My jobs", "my jobs"));
+        posts.add(new Post("Test", "this is a test"));
+    }
+
     @GetMapping("/posts")
-    @ResponseBody
-    public String postsIndex() {
-        return "posts index page";
+    public String postsIndex(Model model) {
+        model.addAttribute("posts", posts);
+        return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    @ResponseBody
-    public String postView() {
-        return "view an individual post";
+
+    public String postView(Model model) {
+        Post post = new Post("test", "this is a test");
+        model.addAttribute("post", post);
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
