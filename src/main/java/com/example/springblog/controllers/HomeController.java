ecf8047a19ea8b37b18966f9b2b21c627b62e2e3
Kathleen Weaver <kathleen@kweaver.org>
2021-04-30 09:53:30 -0500

    added Exercise 1

--- src/main/java/com/example/springblog/controllers/HomeController.java
+++ src/main/java/com/example/springblog/controllers/HomeController.java
@@ -1,4 +1,14 @@
 package com.example.springblog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String landing() {
+        return "This is the landing page";
+    }
 }
