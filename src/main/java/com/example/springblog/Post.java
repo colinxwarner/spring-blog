9fda700a6e96471dbb31c52fce7636c796763285
Kathleen Weaver <kathleen@kweaver.org>
2021-04-30 14:07:40 -0500

    added post class

---
+++ src/main/java/com/example/springblog/Post.java
@@ -0,0 +1,31 @@
+package com.example.springblog;
+
+public class Post {
+
+    private String title;
+    private String body;
+
+    public Post() {
+    }
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
