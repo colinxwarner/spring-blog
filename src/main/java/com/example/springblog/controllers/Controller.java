fc9f288189e91dc0b88e51c0b06222608806b232
Kathleen Weaver <kathleen@kweaver.org>
2021-04-30 09:30:43 -0500

    controller using data

--- src/main/java/com/example/springblog/controllers/Controller.java
+++ src/main/java/com/example/springblog/controllers/Controller.java
@@ -3,6 +3,7 @@ package com.example.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -12,5 +13,11 @@ class HelloController {
     @ResponseBody
     public String hello() {
         return "Hello from Spring!";
+    }
+
+    @GetMapping("/hello/{name}")
+    @ResponseBody
+    public String addOne(@PathVariable String name) {
+        return "Hello " + name + "!";
     }
 }
