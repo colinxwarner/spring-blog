e078bc1cfebce992d7c1ec1997a43da05bd082cc
Justin Reich <justinreich.dev@gmail.com>
2021-05-02 17:08:47 -0500

    init commit

---
+++ src/main/java/com/codeup/lunablog/LunaBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.lunablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunaBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunaBlogApplication.class, args);
+    }
+
+}
