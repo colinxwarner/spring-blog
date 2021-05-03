72b504641053c13ec2b6fc4669acf15348b15dff
Justin Reich <justinreich.dev@gmail.com>
2021-05-03 08:27:22 -0500

    feat: Home page to demo views

--- src/main/java/com/codeup/lunablog/controllers/HomeController.java
+++ src/main/java/com/codeup/lunablog/controllers/HomeController.java
@@ -2,13 +2,11 @@ package com.codeup.lunablog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    @ResponseBody
     public String welcome() {
-        return "This is the landing page!";
+        return "home";
     }
 }
