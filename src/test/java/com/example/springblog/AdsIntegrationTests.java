879903dfdb6a9955daf2eadfdbf475ab750a0860
Kathleen Weaver <kathleen@kweaver.org>
2021-05-05 10:24:28 -0500

    added integration test

---
+++ src/test/java/com/example/springblog/AdsIntegrationTests.java
@@ -0,0 +1,67 @@
+package com.example.springblog;
+
+import com.example.springblog.models.User;
+import com.example.springblog.repositories.AdRepository;
+import com.example.springblog.repositories.Users;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.servlet.http.HttpSession;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringblogApplication.class)
+@AutoConfigureMockMvc
+public class AdsIntegrationTests {
+
+    private User testUser;
+    private HttpSession httpSession;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    Users userDao;
+
+    @Autowired
+    AdRepository adsDao;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Before
+    public void setup() throws Exception {
+
+        testUser = userDao.findByUsername("testUser");
+
+        // Creates the user if they don't exist
+
+        if(testUser == null) {
+            User newUser = new User();
+            newUser.setUsername("testUser");
+            newUser.setPassword(passwordEncoder.encode("pass"));
+            newUser.setEmail("testuser@codeup.com");
+        }
+
+// Throws a Post request to /login and expect a redirection to the Ads index page after being logged in
+        httpSession = this.mvc.perform(post("/login").with(csrf())
+                .param("username", "testUser")
+                .param("password", "pass"))
+                .andExpect(status().is(HttpStatus.FOUND.value()))
+                .andExpect(redirectedUrl("/ads"))
+                .andReturn()
+                .getRequest()
+                .getSession();
+    }
+}
