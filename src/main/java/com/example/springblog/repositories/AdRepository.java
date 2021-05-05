660eb2e57d6f4fed7a21241f08a290404bad19e2
Kathleen Weaver <kathleen@kweaver.org>
2021-05-05 10:34:44 -0500

    added additional tests

--- src/main/java/com/example/springblog/repositories/AdRepository.java
+++ src/main/java/com/example/springblog/repositories/AdRepository.java
@@ -4,5 +4,5 @@ import com.example.springblog.models.Ad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdRepository extends JpaRepository<Ad, Long> {
-
+    Ad findByTitle(String title);
 }
