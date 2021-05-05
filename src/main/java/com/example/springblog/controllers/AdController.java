78dfab0b20be50ba1f73a1228565f05456dddf30
Kathleen Weaver <kathleen@kweaver.org>
2021-05-05 09:35:46 -0500

    completed examples

--- src/main/java/com/example/springblog/controllers/AdController.java
+++ src/main/java/com/example/springblog/controllers/AdController.java
@@ -1,10 +1,12 @@
 package com.example.springblog.controllers;
 
+import com.example.springblog.models.Ad;
+import com.example.springblog.models.User;
 import com.example.springblog.repositories.AdRepository;
+import com.example.springblog.repositories.Users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class AdController {
@@ -24,5 +26,25 @@ public class AdController {
     public String getOne(@PathVariable long id, Model model) {
         model.addAttribute("ad", adDao.getOne(id));
         return "ads/show";
+    }
+
+    @GetMapping("/ads/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("ad", new Ad());
+        return "ads/create";
+    }
+
+    @PostMapping("/ads/create")
+    @ResponseBody
+    public String create(
+            @RequestParam(name = "title") String title,
+            @RequestParam(name = "description") String description
+    ) {
+        Ad ad = new Ad();
+        ad.setTitle(title);
+        ad.setDescription(description);
+
+        adDao.save(ad);
+        return "Ad saved!";
     }
 }
